89da74076bece55e89f374a731487ef3da56eb60
SeayPrem <seayprem@hotmail.com>
2021-09-15 19:31:28 +0700

    Updated check user already exists

--- admin/controller/officerAdd.php
+++ admin/controller/officerAdd.php
@@ -12,12 +12,19 @@ if(isset($_POST['add'])) {
 	$salt = 'a2a551a6458a8de22446cc76d639a9e9';
 	$hash = sha1($salt.$pass);
 
-	$sql = "INSERT INTO officers (officer_id, officer_user, officer_pass, officer_fname, officer_lname, officer_tel) VALUES ('".$id."', '".$user."', '".$hash."', '".$fname."', '".$lname."', '".$tel."')";
-	$query = mysqli_query($conn, $sql);
-	if($query) {
-		header("Location: ../officers.php");
+	$check_sql = "SELECT * FROM officers WHERE officer_user = '".$user."'";
+	$check_query = mysqli_query($conn, $check_sql);
+	if(mysqli_num_rows($check_query) > 0) {
+		echo "Already Exists";
 	} else {
-		echo '<script>alert("ล้มเหลว กรุณาลองใหม่อีกครั้ง"); window.location.href="../officers.php"</script>';
+		$sql = "INSERT INTO officers (officer_id, officer_user, officer_pass, officer_fname, officer_lname, officer_tel) VALUES ('".$id."', '".$user."', '".$hash."', '".$fname."', '".$lname."', '".$tel."')";
+		$query = mysqli_query($conn, $sql);
+		if($query) {
+			header("Location: ../officers.php");
+		} else {
+			echo '<script>alert("ล้มเหลว กรุณาลองใหม่อีกครั้ง"); window.location.href="../officers.php"</script>';
+		}
 	}
+
 }
 ?>
