37f73d80e566f1dab3103718f6bde2e646b3bf73
SeayPrem <seayprem@hotmail.com>
2021-09-27 22:15:04 +0700

    Change name db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'exam';
+$dbname = 'systemexam';
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 mysqli_set_charset($conn, 'utf8');
